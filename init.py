94e8d7522d7ce105e2ae53eef562c284d99a5063
Kevin <cifuentesclud@gmail.com>
2023-10-23 21:03:16 -0500

    agregofuncionprincipal

---
+++ init.py
@@ -0,0 +1,97 @@
+from operations import add
+from division import divisionIn
+from potencia import power
+from Resta import resta
+from Multiplicacion import multiplicacion
+
+
+def game():
+    score = 0
+    
+    
+    while True:
+        print('======== Menu ========'
+        '\n1. Add'
+        '\n2. resta'
+        '\n3. multiplicacion'
+        '\n4. division'
+        '\n5. potencia'
+        '\n6. modulo'
+        '\n0. Exit')
+        option = int(input('\nChoice an option: '))
+        if option == 0:
+            break
+        num_1 = float(input('Enter first number: '))
+        num_2 = float(input('Enter second number: '))
+        answer = float(input('Enter you answer: '))
+        if option == 1:
+            resultf = add(num_1, num_2)
+
+            if resultf == answer:
+
+                score += 1
+                print('Correct!!')
+        else:
+            print('Incorrect')
+
+
+        if option == 2:
+            resultf = add(num_1, num_2)
+
+            if resultf == answer:
+
+                score += 1
+                print('Correct!!')
+        else:
+            print('Incorrect')
+
+        
+        if option == 3:
+            resultf = add(num_1, num_2)
+
+            if resultf == answer:
+
+                score += 2
+                print('Correct!!')
+        else:
+            print('Incorrect')
+
+
+        if option == 4:
+            resultf = divisionIn(num_1, num_2)
+
+            if resultf == answer:
+
+                score += 2
+                print('Correct!!')
+        else:
+            print('Incorrect')
+
+
+        if option == 5:
+            resultf = power(num_1, num_2)
+            
+            if resultf == answer:
+
+                score += 4
+            print('Correct!!')
+        else:
+            print('Incorrect')
+
+        
+        if option == 6:
+            resultf = power(num_1, num_2)
+            
+            if resultf == answer:
+
+                score += 4
+            print('Correct!!')
+        else:
+            print('Incorrect')
+    
+
+
+    print(f'======== Game Over ========'
+    f'\nYou score is {score}'
+    '\nKeep going!')
+game()
--- potencia.py
+++
@@ -1,7 +0,0 @@
-def power(num_1, num_2):
-
-    result = float(num_1 ** num_2)
-
-    print(num_1, '^' ,num_2, 'is equal to', result)
-    
-    return result
